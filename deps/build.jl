dabb6b37da60be8a21a2891237d5e684a75db5f9
Marius Meyer <mariusme@mail.upb.de>
2024-08-21 11:44:20 +0200

    Add Libuuid_jll as dependency
    Instead of using system library, which may fail on some systems

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,7 @@ using Pkg
 using Scratch
 using xrt_jll
 using Logging
+using Libuuid_jll
 
 uuid = Base.UUID(Pkg.TOML.parsefile("../Project.toml")["uuid"])
 #@show keys(Pkg.TOML.parsefile(joinpath(dirname(@__DIR__), "../Project.toml")))
@@ -12,13 +13,20 @@ if isdir(build_dir())
     rm(build_dir(), force=true, recursive=true)
 end
 
-cmake_opts = ""
+get_version(xbutil_version) = VersionNumber(match(r"Version\s+:\s+(\d+\.\d+\.\d+)", xbutil_version)[1])
+
+xbutil_version = VersionNumber("1")
+
+@info "Use Libuuid in $(Libuuid_jll.artifact_dir)"
+cmake_opts = ["-DLIB_UUID_DIR=$(Libuuid_jll.artifact_dir)"]
 if "XILINX_XRT" in keys(ENV)
     @info "Build using native XRT at $(ENV["XILINX_XRT"])"
-    cmake_opts = "-DXILINX_XRT=$(ENV["XILINX_XRT"])"
+    push!(cmake_opts,"-DXILINX_XRT=$(ENV["XILINX_XRT"])")
+    # xbutil_version = get_version(read(`xbutil --version`, String))
 else
     @info "Build using xrt_jll"
-    cmake_opts = "-DXILINX_XRT=$(xrt_jll.artifact_dir)"
+    push!(cmake_opts, "-DXILINX_XRT=$(xrt_jll.artifact_dir)")
+    # xbutil_version = get_version(read(`$(xrt_jll.artifact_dir)/bin/xbutil --version`, String))
 end
 
 mkdir(build_dir())

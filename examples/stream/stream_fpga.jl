9252a7f4c8d67b078a1e0bf414a5fd4d55ba1f37
Marius Meyer <marius.meyer@uni-paderborn.de>
2024-09-04 17:18:53 +0200

    Measure time and bandwidth in stream

--- examples/stream/stream_fpga.jl
+++ examples/stream/stream_fpga.jl
@@ -2,13 +2,20 @@ using XRT
 using ArrayAllocators
 using Logging
 
+array_size = 2^20
+
+if !("XCL_EMULATION_MODE" in keys(ENV))
+    # Hardware execution. Increase array array_size
+    array_size = 2^28
+end
+
 # Allocate an output array
 @info "Allocate and initialize arrays"
-a = Array{Float64}(MemAlign(4096),2^20)
-b = Array{Float64}(MemAlign(4096),2^20)
-c = Array{Float64}(MemAlign(4096),2^20)
+a = Array{Float64}(MemAlign(4096),array_size)
+b = Array{Float64}(MemAlign(4096),array_size)
+c = Array{Float64}(MemAlign(4096),array_size)
 
-for i in 1:2^20
+for i in 1:array_size
     a[i] = rand()
     b[i] = rand()
     c[i] = 0
@@ -20,8 +27,15 @@ end
 bs = XRT.prepare_bitstream("build_sw_emu/stream.xclbin")
 
 # execute the stream kernel
-@info "Execute kernel" 
-bs.stream_calc!(a, b, c, 2.0, 2^20, 1)
+@info "Execute kernel test run" 
+bs.stream_calc!(a, b, c, 2.0, 16, 1)
+c .= 0.0
+
+@info "Execute full kernel run TRIAD" 
+execution_time = @elapsed bs.stream_calc!(a, b, c, 2.0, array_size, 1)
+
+@info "Execution time: $execution_time seconds"
+@info "Measured bandwidth: $((3 * array_size * sizeof(eltype(a))) / execution_time * 1.0e-9) GB/s"
 
 # validate the execution results
 @info "Validate output"

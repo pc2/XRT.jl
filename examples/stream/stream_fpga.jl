759bcfed395fb6a976b36cd9e313408893f2f645
Marius Meyer <marius.meyer@uni-paderborn.de>
2024-09-04 16:10:13 +0200

    Add logging to stream example

--- examples/stream/stream_fpga.jl
+++ examples/stream/stream_fpga.jl
@@ -1,7 +1,9 @@
 using XRT
 using ArrayAllocators
+using Logging
 
 # Allocate an output array
+@info "Allocate and initialize arrays"
 a = Array{Float64}(MemAlign(4096),2^20)
 b = Array{Float64}(MemAlign(4096),2^20)
 c = Array{Float64}(MemAlign(4096),2^20)
@@ -14,10 +16,15 @@ end
 
 # Load the bitstream to the FPGA and generate functions 
 # for each kernel
+@info "Upload bitstream and generate kernel functions"
 bs = XRT.prepare_bitstream("build_sw_emu/stream.xclbin")
 
 # execute the stream kernel
+@info "Execute kernel" 
 bs.stream_calc!(a, b, c, 2.0, 2^20, 1)
 
 # validate the execution results
+@info "Validate output"
 @assert all(c .== (2 .* a .+ b))
+
+@info "Done"
